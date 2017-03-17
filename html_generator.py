c3482b6bf2459fecc6664a67a87a4c879feba013
RandyHoo <ryozero1996@yahoo.co.jp>
2017-03-17 23:39:20 +0900

    Add files via upload

--- html_generator.py
+++ html_generator.py
@@ -19,12 +19,6 @@ foot_path = os.path.normpath(os.path.join(base, 'template/foot.html'))
 #出力先のパス
 output_path = os.path.normpath(os.path.join(base, 'output/'))
 
-
-#フォーマットを開く
-head = codecs.open( head_path, 'r','shift_jis' )
-side = codecs.open( side_path, 'r','shift_jis' )
-foot = codecs.open( foot_path, 'r','shift_jis' )
-
 #本文のファイル名のみを取得し、ループを回して記事を生成する
 filename_list = [f for f in content_lists if os.path.isfile(os.path.join(content_path, f))]
 for val in filename_list:
@@ -33,6 +27,11 @@ for val in filename_list:
     os.chdir("./content")
     content = codecs.open( val, 'r','shift_jis' )
     os.chdir(base)  #書き込み処理のために親ディレクトリに戻る
+    
+    #フォーマットを開く
+    head = codecs.open( head_path, 'r','shift_jis' )
+    side = codecs.open( side_path, 'r','shift_jis' )
+    foot = codecs.open( foot_path, 'r','shift_jis' )
     
     #結合のためバッファに格納
     headbuf = head.read()
@@ -47,5 +46,6 @@ for val in filename_list:
     os.chdir("./output")
     fout = codecs.open( val, 'w','utf-8' ) #吐き出すファイル名は入力と同じ
     fout.write(outbuf)
+    content.close()
     fout.close()
     os.chdir(base)  #次周期のために親ディレクトリに戻る
